ad046a299f507a91a83247e6c10d8007806a644f
ChikaLong <longb.0818@gmail.com>
2021-10-09 00:37:25 +0000

    [Add]検索

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,54 @@
+class SearchesController < ApplicationController
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+
+  private
+  def perfect(model, content)
+    if model == 'user'
+      User.where(name: content)
+    elsif model == 'write'
+      Write.where(title: content)
+    end
+  end
+
+  def forward(model, content)
+    if model == "user"
+      User.where("name LIKE ?", "#{content}%")
+    elsif model == "write"
+      Write.where("title LIKE ?", "#{content}%")
+    end
+  end
+
+  def backward(model, content)
+    if model == "user"
+      User.where("name LIKE ?", "%#{content}")
+    elsif model == "write"
+      Write.where("title LIKE ?", "%#{content}")
+    end
+  end
+
+  def partial(model, content)
+    if model == "user"
+      User.where("name LIKE ?", "%#{content}%")
+    elsif model == "write"
+      Write.where("title LIKE ?", "%#{content}%")
+    end
+  end
+
+  def search_for(model, content, method)
+    case method
+    when 'perfect'
+      perfect(model, content)
+    when 'forward'
+      forward(model, content)
+    when 'backward'
+      backward(model, content)
+    when 'partial'
+      partial(model, content)
+    end
+  end
+end
